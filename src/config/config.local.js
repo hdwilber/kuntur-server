bd9eae4bdaf71097e7c94d2613e4228f0e203591
hdwilber <hd.wilber@gmail.com>
2017-11-21 17:40:30 -0400

    Improved Explorer Model. Setting up mailer and configuration for port and host

---
+++ src/config/config.local.js
@@ -0,0 +1,4 @@
+export const { 
+  APP_HOST: host, 
+  APP_PORT: port } = process.env
+
--- src/models/explorer.js
+++ src/models/explorer.js
@@ -1,3 +1,6 @@
+import Mailer from '../lib/mail'
+import crypto from 'crypto'
+
 export default function (Explorer) {
 
   Explorer.beforeRemote ('**', (context, instance, next) => {
@@ -10,8 +13,77 @@ export default function (Explorer) {
     context.instance.updated = now
     if (context.isNewInstance) {
       context.instance.created = now
+      crypto.randomBytes(64, (error, buf) => {
+        if (!error && buf) {
+          context.instance.verificationToken = buf.toString('hex')
+          context.instance.verifiedEmail = false
+          next()
+        }
+      });
+    } else {
+      next()
+    }
+  })
+
+  Explorer.observe ('after save', (context, next) => {
+    next()
+    if (context.isNewInstance) {
+      Mailer.send({
+        recipientEmail: 'hd.wilber@gmail.com',
+        token: context.instance.verificationToken,
+        verificationUrl: `localhost/Explorers/confirm?uid=${context.instance.id}&token=${context.instance.verificationToken}`
+      })
+      .then (res => {
+        next()
+      })
+      .catch (error => {
+        next(error)
+      })
+    }
+  })
+
+  Explorer.confirm = function (uid, token, redirect, next) {
+    Explorer.findById(uid, (error, explorer ) => {
+      if (!error && explorer) {
+        const clearToken = token.trim()
+
+        if (explorer.verificationToken === clearToken) {
+          explorer.verifiedEmail = true
+          explorer.verificationToken = null
+          explorer.save( function (error, newExplorer) {
+            if (!error) {
+              next()
+            } else {
+              next(error) 
+            }
+          })
+        } else {
+          next(new Error('Something went wrong'))
+        }
+      } else {
+        next(error)
+      }
+    })
+  }
+
+  Explorer.afterRemote ('confirm', (context, instance, next) => {
+    context.result = {
+      message: 'Email verified'
     }
     next()
   })
 
+  Explorer.remoteMethod(
+    'confirm',
+    {
+      description: 'Confirm a user registration with identity verification token.',
+      accepts: [
+        {arg: 'uid', type: 'string', required: true},
+        {arg: 'token', type: 'string', required: true},
+        {arg: 'redirect', type: 'string'},
+      ],
+      returns: {arg: 'data', type: 'object'},
+      http: {verb: 'get', path: '/confirm'},
+    }
+  );
 }
